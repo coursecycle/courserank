8b1470153dd2d811bb3aac4b56a3fec345a52baf
Roger Chen <rogerthechen@gmail.com>
2014-11-16 12:27:21 -0800

    Multithreaded with eight threads, performance is awesome now

--- scrape.rb
+++ scrape.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'mechanize'
 require 'mongo'
 require 'nokogiri'
+require 'thread'
 
 include Mongo
 
@@ -13,13 +14,16 @@ COMMENTS_BASE_URL = "https://www.courserank.com/stanford/target/comment_sort"
 username = ask("Username: ") { |q| q.echo = true }
 password = ask("Password: ") { |q| q.echo = "*" }
 
-m = Mechanize.new
-m.user_agent = 'Mac Safari'
-m.get(LOGIN_FORM) do |page|
-    page.form_with(:id => 'loginForm') do |f|
-        f.username = username
-        f.password = password
-    end.click_button
+def getMechanizeInstance(username, password)
+    m = Mechanize.new
+    m.user_agent = 'Mac Safari'
+    m.get(LOGIN_FORM) do |page|
+        page.form_with(:id => 'loginForm') do |f|
+            f.username = username
+            f.password = password
+        end.click_button
+    end
+    return m
 end
 
 def getClassContents(m, id)
@@ -103,13 +107,27 @@ def getClassContents(m, id)
 
 end
 
+queue = Queue.new
+(1..50000).to_a.each{|x| queue.push x}
+
 client = MongoClient.new
 db = client["courseriver"]
-collection = db["courserank"]
-(1..40000).each do |i|
-    contents = getClassContents(m, i.to_s)
-    unless contents.nil?
-        id = collection.insert(contents)
-        puts "Inserted " + i.to_s + " at " + id.to_s
+collection = db["courserank-th"]
+
+m = getMechanizeInstance(username, password)
+workers = (0...8).map do
+    Thread.new do
+        begin
+            while x = queue.pop(true)
+                contents = getClassContents(m, x.to_s)
+                unless contents.nil?
+                    id = collection.insert(contents)
+                    puts "Inserted " + x.to_s + " at " + id.to_s
+                end                
+            end
+        rescue ThreadError
+        end
     end
-end
+end; "ok"
+workers.map(&:join); "ok"
+
